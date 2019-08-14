04bc15e1fd12337f4c5bf2c28fdec9df47f21bcd
Maxime GELLE <maxime.gelle@gmail.com>
2019-08-14 12:58:25 +0200

    Add debit / credit in statement

--- src/bank_account/core.clj
+++ src/bank_account/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as string]))
 
-(defrecord Operation [date amount])
+(defrecord Operation [date amount type])
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -11,27 +11,34 @@
 
 (defn newAccount [] '())
 
-(defn new-operation [date amount] (Operation. date amount))
+(defn new-operation [date amount type] (Operation. date amount type))
 
 (defn deposit [account amount now]
-  (cons (new-operation (now) amount) account))
+  (cons (new-operation (now) amount "CREDIT") account))
 
 (defn withdraw [account amount now]
-  (cons (new-operation (now) (- amount)) account))
+  (cons (new-operation (now) amount "DEBIT") account))
+
+(defn to-amount [operation]
+  (if (= (:type operation) "DEBIT")
+      (- (.amount operation))
+      (.amount operation)))
 
 (defn sum [numbers] (reduce + 0 numbers))
 
 (defn balance [account]
   (->> account
-       (map :amount)
+       (map to-amount)
        (sum)))
 
 (defn statement-line [operation]
-  (str (.date operation) " | " (.amount operation)))
+  (if (= (:type operation) "DEBIT")
+  (str (.date operation) " |  | " (.amount operation))
+  (str (.date operation) " | " (.amount operation) " | ")))
 
 (defn print-statement [account]
   (->> account
        (reverse)
        (map statement-line)
        (string/join "\n")
-       (str "Date | Amount\n")))
+       (str "Date | Credit | Debit\n")))
