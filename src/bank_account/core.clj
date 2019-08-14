95ccebe1718277ad21a1a569564407ba83ffcb97
Maxime GELLE <maxime.gelle@gmail.com>
2019-08-14 14:53:28 +0200

    display the balance

--- src/bank_account/core.clj
+++ src/bank_account/core.clj
@@ -33,15 +33,30 @@
        (map to-amount)
        (sum)))
 
-(defn statement-line [operation]
+(defn statement-line [operation balance]
   (let [amount (.amount operation) date (.date operation)]
     (match [(:type operation)]
-      ["CREDIT"] (str date " | " amount " | ")
-      ["DEBIT"] (str date " |  | " amount))))
+      ["CREDIT"] (str date " | " amount " |  | " balance)
+      ["DEBIT"] (str date " |  | " amount " | " balance))))
+
+(defn add-operation-to-balance [balance operation] 
+  (+ balance (to-amount operation)))
+
+(defn add-operation-to-statement-lines [statementLines operation balance] 
+  (cons (statement-line operation balance) statementLines))
+
+(defn agregate [balancedOperations operation]
+  (let [balance (:balance balancedOperations) 
+        statementLines (:statementLines balancedOperations)
+        newBalance (add-operation-to-balance balance operation)
+        newStatementLines (add-operation-to-statement-lines statementLines operation newBalance)]
+      { :balance newBalance :statementLines newStatementLines}))
 
 (defn print-statement [account]
   (->> account
        (reverse)
-       (map statement-line)
+       (reduce agregate {:balance 0 :statementLines '()})
+       (:statementLines)
+       (reverse)
        (string/join "\n")
-       (str "Date | Credit | Debit\n")))
+       (str "Date | Credit | Debit | Balance\n")))
