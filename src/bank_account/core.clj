8a15c074dfe2e163720a1c42db2aad262c14bce0
Maxime GELLE <maxime.gelle@gmail.com>
2019-08-17 16:41:06 +0200

    Add main

--- src/bank_account/core.clj
+++ src/bank_account/core.clj
@@ -1,7 +1,14 @@
 (ns bank-account.core
-  (:gen-class))
+  (:gen-class)
+  (:require [bank-account.account.account :refer :all]
+            [bank-account.account.statement :refer :all]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (-> (newAccount)
+       (deposit 1000 (fn [] "01-01-2019"))
+       (withdraw 500 (fn [] "02-01-2019"))
+       (deposit 300 (fn [] "03-01-2019"))
+       (print-statement)
+       (println)))
